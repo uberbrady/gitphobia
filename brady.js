5b9663fba0f980b1ae3b841eee7b072636a5d3fe
snipe <snipe@snipe.net>
2012-09-22 19:05:48 -0400

    bitches.

--- brady.js
+++ brady.js
@@ -13,6 +13,6 @@ if (xml_data['page']['characterInfo']['attr']['errCode']=='noCharacter'){
 	sql = "update wow_toons set valid_toon='n' where toon_id='".toons[i]['toon_id']."'";
 	mark = mysql_query(sql);
 	fbml .=  mysql_error();
-	fbml .='<li>'.toons[i]['toon_name'].' marked as invalid';	
+	fbml .='<li>'.toons[i]['toon_name'].' marked as invalid, bitches';	
 	invalid_count++;
 }
