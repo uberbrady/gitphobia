ac61a33a8493f9c3d582930f10ca4ff787d2d904
snipe <snipe@snipe.net>
2012-09-22 18:57:18 -0400

    fixed indents

--- brady.php
+++ brady.php
@@ -5,15 +5,15 @@
 //it's MINE.
 
 if ($xml_data['page']['characterInfo']['attr']['errCode']=='noCharacter'){
-$sql = "delete from wow_toons where toon_id='".$toons[$i]['toon_id']."'";
-$mark = mysql_query($sql);
-$fbml .=  mysql_error();	
-$fbml .='<li>'.$toons[$i]['toon_name'].' Deleted';
-$del_count++;
+	$sql = "delete from wow_toons where toon_id='".$toons[$i]['toon_id']."'";
+	$mark = mysql_query($sql);
+	$fbml .=  mysql_error();	
+	$fbml .='<li>'.$toons[$i]['toon_name'].' Deleted';
+	$del_count++;
 } else {
-$sql = "update wow_toons set valid_toon='n' where toon_id='".$toons[$i]['toon_id']."'";
-$mark = mysql_query($sql);
-$fbml .=  mysql_error();
-$fbml .='<li>'.$toons[$i]['toon_name'].' marked as invalid';	
-$invalid_count++;
+	$sql = "update wow_toons set valid_toon='n' where toon_id='".$toons[$i]['toon_id']."'";
+	$mark = mysql_query($sql);
+	$fbml .=  mysql_error();
+	$fbml .='<li>'.$toons[$i]['toon_name'].' marked as invalid';	
+	$invalid_count++;
 }
