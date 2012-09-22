f466a0d18dbedae6b002168723d12407c09bb31a
snipe <snipe@snipe.net>
2012-09-22 18:37:08 -0400

    breaking

--- sample.php
+++ sample.php
@@ -13,3 +13,5 @@ echo $jackylpuss;
 
 mysql_connect("something","something","something");
 
+$base_url = base_toon_url($toons[$i]['realm'], $toons[$i]['toon_name'], $toons[$i]['country']);
+$fbml .= '<li><b><a href="'.$url.'">'.$toons[$i]['toon_name'].'</a></b> ('.$toons[$i]['realm'].')';
