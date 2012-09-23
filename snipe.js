dd94e52f9271e098ec9576a435c3d7fd4a11f7cb
snipe <snipe@snipe.net>
2012-09-22 22:17:32 -0400

    Fixed ferreals this time.

--- snipe.js
+++ snipe.js
@@ -20,4 +20,4 @@ include(app_path.'/inc/lib.php');
 var sql = "select distinct(fb_id) as fb_id, toon_name from wow_toons where (toon_lvl IS NULL OR toon_lvl='') AND valid_toon='y' order by toon_name asc limit 15";
 
 console.log('this change fixes my bad hotfix from before that was reverted.');
-console.log('this change is another bad fix that we will need to revert. I HATE APOSTROPHES!);
+console.log('this change fixes the fix of the fix of the bad thing. I think.');
